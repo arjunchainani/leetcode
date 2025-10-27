fb1ed2a2098559b838ca799c0f77c34ec8a7ccdc
Arjun Chainani <109640014+arjunchainani@users.noreply.github.com>
2025-10-27 15:42:42 -0500

    Time: 129 ms (7.51%) Space: 20.7 MB (86.54%)

--- 0011-container-with-most-water/container-with-most-water.py
+++ 0011-container-with-most-water/container-with-most-water.py
@@ -1,16 +1,6 @@
-# maximize area -> base -- second i value minus first i value
-# height -- lowest height between the two values
-# BCR: O(n)
-
-
-# pass through the array
-# two pointers --> one of them corresponds to the left wall of the container and the second corresponds to the right wall
-#      init at (0, 1)
-#      variable to store current largest area --> return this at the end
-#      while ptrs dont get to (n-1, n)
-#            increment ptr with lower height and check if you have a new highest area
-#                if not, keep incrementing until you hit the end of the array and checking
-#                if you do, update the current largest area var, and then repeat the inner loop
+# use two pointers starting at each end because that gives you optimal width,
+# and then you adjust smaller point for height vs width trade-off
+# considers all potentially optimal pairs without needing to check every pair
 
 class Solution(object):
     def maxArea(self, height):
