d267dacb4ddbe2b323f5cbc6c915e9a6efa89d22
Arjun Chainani <109640014+arjunchainani@users.noreply.github.com>
2026-01-06 19:42:24 -0600

    Time: 162 ms (61.60%) Space: 26.9 MB (56.10%)

--- 0739-daily-temperatures/daily-temperatures.py
+++ 0739-daily-temperatures/daily-temperatures.py
@@ -1,4 +1,4 @@
-# use a monotonically decreasing stack, at the end of each iteration through a daily temp, whatever day indices are still on the stack should have their answer[i] incremented
+# use a monotonically decreasing stack, whenever you pop an index off, update that answer[index] with the difference between the current index (i) and that popped index
 # make sure that stack stores the indices of the days and not the actual temp, but compare using the actual temp
 # at the end of the loop, whichever indices are still on the stack should have their answer[i] set to 0 (since no warmer temp was ever found)
 
